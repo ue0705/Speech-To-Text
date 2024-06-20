e9409f94e4bae9ef9ec58cd650da89b334f7808a
ue0705 <117436583+ue0705@users.noreply.github.com>
2024-06-20 09:50:22 +0800

    Update audio_google.py

--- audio_google.py
+++ audio_google.py
@@ -14,11 +14,9 @@ LINE_NOTIFY_TOKEN = 'LINE_NOTIFY_TOKEN'
 AUDIO_FILE = "./audio_record/recorded_audio" #.wav
 
 def send_line_notify(message): #使用 Line Notify 傳送訊息
-    headers = {
-        "Authorization": f"Bearer {LINE_NOTIFY_TOKEN}"
-    }
+    headers = {"Authorization": f"Bearer {LINE_NOTIFY_TOKEN}"}
     payload = {"message": message}
-    response = requests.post("https://notify-api.line.me/api/notify", headers=headers, data=payload)
+    response = requests.post("https://notify-api.line.me/api/notify", headers=headers, data={"message": message})
     return response
 
 def check_emergency_msg(message):
@@ -37,10 +35,9 @@ def check_emergency_msg(message):
 
 def log_message(message, file_path=None): #打印並記錄帶有日期時間的日誌訊息
     log = f"[datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")-Go] {message}"
-    print(log)
-    send_line_notify(log)
-    #check mayday!
-    check_emergency_msg(message)
+    print(log) #local print
+    send_line_notify(log) #send log to line    
+    check_emergency_msg(message) #check mayday!
 
 def recognize_speech_from_input(): #(device_index): #從指定音訊設備捕捉語音並轉換為文字
     r = sr.Recognizer()
@@ -49,7 +46,7 @@ def recognize_speech_from_input(): #(device_index): #從指定音訊設備捕捉
         r.adjust_for_ambient_noise(source, duration=1) #噪音控制 (source, duration=1)
         audio = r.listen(source)
     
-    #get time stamp
+    # get time stamp
     timestamp = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
     
     # 保存音訊文件
@@ -61,19 +58,15 @@ def recognize_speech_from_input(): #(device_index): #從指定音訊設備捕捉
     try:
         text = r.recognize_google(audio, language="zh-TW") # 認識語音（這裡預設語言為中文)
         log_message(message)
-    except sr.RequestError:
-        # API 無法使用
-        log_message("API 無法使用。")
+    except sr.RequestError:        
+        log_message("API 無法使用。") # API 無法使用
     except sr.UnknownValueError:
-        # 無法辨識語音 #log_message("無法辨識語音。")
-        print(f"[{timestamp}]<Local>: 無法辨識語音")
+        print(f"[{timestamp}]<Local>: 無法辨識語音") # 無法辨識語音
 
 def list_audio_devices(): #列出所有音訊輸入設備並返回設備索引
     microphone_list = sr.Microphone.list_microphone_names()
-    print("-----------------------")
     for i, microphone_name in enumerate(microphone_list):
         print(f"Device {i}: {microphone_name}")
-    print("-----------------------")
     return microphone_list
 
 if __name__ == "__main__":
@@ -82,8 +75,6 @@ if __name__ == "__main__":
     log_message("--- 開始執行中文語音轉文字(Recognize_Google), 版本 1.2 ---")
     while True:
         recognize_speech_from_input() #(device_index)
-        # 刪除臨時的音訊文件
-        # if os.path.exists(AUDIO_FILE):
+        # if os.path.exists(AUDIO_FILE): # 刪除臨時的音訊文件
         #    os.remove(AUDIO_FILE)
-        # 加入一個短暫的延遲，避免無限循環過快
-        time.sleep(0.1)
+        time.sleep(0.1) # 加入一個短暫的延遲，避免無限循環過快
